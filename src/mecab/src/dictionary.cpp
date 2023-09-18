dbb7b3da3660f2fdfe2751a997d317196d0ba054
Magic-Xin <x.yu2000@163.com>
2023-09-19 05:45:32 +0800

    Fixed compatibility issues under Xcode 15.0

--- src/mecab/src/dictionary.cpp
+++ src/mecab/src/dictionary.cpp
@@ -65,7 +65,7 @@ int progress_bar_darts(size_t current, size_t total) {
 }
 
 template <typename T1, typename T2>
-struct pair_1st_cmp: public std::binary_function<bool, T1, T2> {
+struct pair_1st_cmp: public std::__binary_function<bool, T1, T2> {
   bool operator()(const std::pair<T1, T2> &x1,
                   const std::pair<T1, T2> &x2)  {
     return x1.first < x2.first;
